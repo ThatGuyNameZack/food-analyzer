fe48bbeb99d3e765f7de97e8ad5a1a5687002159
Zack <rafisatria090805@gmail.com>
2025-05-31 19:40:33 +0700

    tweak and added
    added nutritional value, and added a preview image when uploaded.

---
+++ label.py
@@ -0,0 +1,6 @@
+
+labels = [
+'Ayam Goreng', 'Burger', 'French Fries', 'Gado-Gado', 'Ikan Goreng',
+    'Mie Goreng', 'Nasi Goreng', 'Nasi Padang', 'Pizza',
+    'Rawon', 'Rendang', 'Sate', 'Soto'
+]
---
+++ nutrition.py
@@ -0,0 +1,17 @@
+# nutrition.py
+
+nutrition_data = {
+    'Ayam Goreng': {'calories': 250, 'proteins': 20, 'fat': 15, 'carbohydrates': 5},
+    'Burger': {'calories': 295, 'proteins': 17, 'fat': 13, 'carbohydrates': 33},
+    'French Fries': {'calories': 312, 'proteins': 3.4, 'fat': 15, 'carbohydrates': 41},
+    'Gado-Gado': {'calories': 350, 'proteins': 12, 'fat': 22, 'carbohydrates': 28},
+    'Ikan Goreng': {'calories': 270, 'proteins': 22, 'fat': 18, 'carbohydrates': 3},
+    'Mie Goreng': {'calories': 390, 'proteins': 8, 'fat': 14, 'carbohydrates': 56},
+    'Nasi Goreng': {'calories': 450, 'proteins': 10, 'fat': 16, 'carbohydrates': 60},
+    'Nasi Padang': {'calories': 550, 'proteins': 25, 'fat': 25, 'carbohydrates': 55},
+    'Pizza': {'calories': 285, 'proteins': 12, 'fat': 10, 'carbohydrates': 36},
+    'Rawon': {'calories': 330, 'proteins': 20, 'fat': 18, 'carbohydrates': 15},
+    'Rendang': {'calories': 490, 'proteins': 30, 'fat': 35, 'carbohydrates': 6},
+    'Sate': {'calories': 320, 'proteins': 24, 'fat': 20, 'carbohydrates': 10},
+    'Soto': {'calories': 280, 'proteins': 18, 'fat': 12, 'carbohydrates': 20}
+}
